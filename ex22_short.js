bb808c7db60ede3ad50e09e5a0f5fc11b104ea7a
Diego Borges <diegobdev@outlook.com>
2017-03-07 08:19:04 -0300

    correção do this no exercício 22

--- ex22_short.js
+++ ex22_short.js
@@ -1,11 +1,13 @@
 var nome  ='Bia', anoNascimento=1981;
 function idade(){
-    return new Date().getFullYear() - anoNascimento;
+    return new Date().getFullYear() - this.anoNascimento;
 }
 var pessoa = {
     nome,//:this.nome,
     anoNascimento,//:this.anoNascimento,
     idade,//:this.idade(),
-    toString(){return `${nome} = ${idade()}`;}
+    toString(){return `${this.nome} = ${this.idade()}`;}
 };
+// pessoa.nome = 'Leo';
+// pessoa.anoNascimento = 2000;
 console.log(pessoa.toString());
